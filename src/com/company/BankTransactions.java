2d669f22b5b3fe67c5f40bac1a07d2403cdefa0a
Sanu <ksanu.37@gmail.com>
2021-06-27 22:00:37 +0530

    Simple change

--- src/com/company/BankTransactions.java
+++ src/com/company/BankTransactions.java
@@ -5,6 +5,8 @@ import com.company.JavaConcepts.CustomException;
 public class BankTransactions {
 
     public static int balanceAfterTransaction(int currentBalance, int deductionAmount) throws CustomException {
+
+        // Change to check remote tracking
             if (currentBalance > deductionAmount) {
                 currentBalance -= deductionAmount;
                 return currentBalance;
